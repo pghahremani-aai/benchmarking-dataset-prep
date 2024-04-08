7f1e9f12574f00dd7c4cb60ca06083d5267b514d
Pegah Ghahremani <pegahghahremani@pegahs-mbp.lan>
2024-04-08 14:30:31 -0700

    This commit introduces the evaluate_model_w_orig_word_list function, which directly passes pre-tokenized words to NER model, facilitating the retrieval of NER tags for each subword/word. This approach aligns with the original text more accurately and eliminates the need for heuristic token-label alignment, enhancing the precision of NER tag assignment.

--- compare_different_ner_models.py
+++ compare_different_ner_models.py
@@ -25,6 +25,7 @@ guiding future developments and refinements in NER model implementations.
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 from datasets import load_dataset
 from seqeval.metrics import precision_score, recall_score, f1_score, classification_report
+import torch
 
 # Load the WNUT2017 test dataset
 wnut2017_dataset = load_dataset("wnut_17", split='test')
@@ -37,16 +38,38 @@ entity_mapping = {
     # Add more mappings as needed
 }
 
-manual_sub_mappings = {
-    'O': 'O',
-    'B-MISC': 'O',  # Assuming no direct match, defaulting to 'O'
-    'I-MISC': 'O',  # Assuming no direct match, defaulting to 'O'
-    'B-PER': 'B-person',
-    'I-PER': 'I-person',
-    'B-ORG': 'B-corporation',  # Assuming 'corporation' is a suitable match for 'ORG'
-    'I-ORG': 'I-corporation',
-    'B-LOC': 'B-location',
-    'I-LOC': 'I-location'
+# dataset tag to model tag mapping for BERT model
+bert_manual_mappings = {
+    'B-person': 'B-PER',
+    'I-person': 'I-PER',
+    'B-location': 'B-LOC',
+    'I-location': 'I-LOC',
+    'B-corporation': 'B-ORG',
+    'I-corporation': 'I-ORG',
+    'B-group': 'B-ORG',  # Assuming groups can be mapped to organizations
+    'I-group': 'I-ORG',
+    'B-creative-work': 'B-MISC',  # Assuming creative works can be considered miscellaneous
+    'I-creative-work': 'I-MISC',
+    'B-product': 'B-MISC',  # Assuming products can be considered miscellaneous
+    'I-product': 'I-MISC',
+    'O': 'O'  # 'O' tag typically remains the same
+}
+
+# dataset tag to model tag mapping for RoBERTa model
+roberta_manual_mapping = {
+    'B-person': 'B-person',
+    'I-person': 'I-person',
+    'B-product': 'B-product',
+    'I-product': 'I-product',
+    'B-group': 'B-group',
+    'I-group': 'I-group',
+    'B-location': 'B-location',
+    'I-location': 'I-location',
+    'B-creative-work': 'B-creative-work',
+    'I-creative-work': 'I-creative-work',
+    'B-corporation': 'B-corporation',
+    'I-corporation': 'I-corporation',
+    'O': 'O'
 }
 
 def compute_metrics(predictions, labels):
@@ -111,6 +134,32 @@ def merge_entities(predicted_entities):
     return merged_entities_mapped
 
 def align_tokens(actual_tokens, pre_tokens, pre_labels):
+    """
+    Aligns predicted labels with tokens from a baseline tokenizer.
+
+    This function addresses the tokenization differences between two models by aligning the labels
+    from the model's predictions (which may use a different tokenizer) with the tokens generated
+    by a baseline tokenizer. It ensures that each token from the baseline tokenizer has an 
+    associated label, which is crucial for accurately evaluating the model's performance.
+
+    The function handles sub-token elements (e.g., tokens prefixed with '##' in BERT tokenizers) 
+    by merging them to reconstruct the original tokens, thus ensuring correct label assignment.
+
+    Parameters:
+    - actual_tokens (list of str): Tokens from the baseline tokenizer.
+    - pre_tokens (list of str): Tokens from the model's tokenizer, which may include sub-tokens.
+    - pre_labels (list of str): Labels assigned by the model, corresponding to pre_tokens.
+
+    Returns:
+    - list of str: Aligned labels for each token in actual_tokens.
+
+    Example:
+    actual_tokens = ["New", "York", "City"]
+    pre_tokens = ["New", "York", "Ci", "##ty"]
+    pre_labels = ["B-LOC", "I-LOC", "I-LOC", "I-LOC"]
+    aligned_labels = align_tokens(actual_tokens, pre_tokens, pre_labels)
+    # aligned_labels will be ["B-LOC", "I-LOC", "I-LOC"]
+    """
     aligned_labels = ['O'] * len(actual_tokens)  # Initialize with 'O' labels
     pre_index = 0  # Index for predicted tokens
 
@@ -120,7 +169,7 @@ def align_tokens(actual_tokens, pre_tokens, pre_labels):
 
         reconstructed_token = ""
         while pre_index < len(pre_tokens):
-            part_of_token = pre_tokens[pre_index].replace("##", "")
+            part_of_token = pre_tokens[pre_index].replace("##", "").replace("Ġ", "")
             reconstructed_token += part_of_token
 
             # Move to the next predicted token if it's part of the current actual token
@@ -172,12 +221,70 @@ def create_mapping(model_tags, dataset_tags):
 
     return mapping
 
+model_name_to_manual_mapping = {
+    'dslim/bert-base-NER': bert_manual_mappings,
+    'dslim/bert-base-NER-uncased': bert_manual_mappings,
+    'Babelscape/wikineural-multilingual-ner': bert_manual_mappings,
+    'tner/roberta-large-wnut2017': roberta_manual_mapping,
+    'dbmdz/bert-large-cased-finetuned-conll03-english': bert_manual_mappings,
+}
+
+def check_model_tags(model_name, dataset):
+        model = AutoModelForTokenClassification.from_pretrained(model_name)
+        model_ner_tags = set(model.config.id2label.values())
+        dataset_ner_tags = set(dataset.features['ner_tags'].feature.names)
+        manual_tags = set(model_name_to_manual_mapping[model_name].values())
+        if model_ner_tags != manual_tags:
+            print(f"Error: The model, {model_name} NER tags do not match the manually defined tags.")
+            print("Model NER Tags:", model_ner_tags, " for model:", model_name)
+            print("Manual NER Tags:", manual_tags)
+        else:
+            print(f"The model, {model_name}, NER tags match the manually defined tags.")
+
+def evaluate_model_w_orig_word_list(model_name, dataset):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=True)
+    model = AutoModelForTokenClassification.from_pretrained(model_name)
+
+    manual_sub_mappings = model_name_to_manual_mapping[model_name]
+    check_model_tags(model_name, wnut2017_dataset)
+
+    predictions = []
+    labels = []
+    for example in dataset:
+        tokens = example['tokens']
+        encoded_input = tokenizer(tokens, is_split_into_words=True, return_tensors="pt")
+
+        with torch.no_grad():
+            output = model(**encoded_input)
+
+        prediction = output.logits.argmax(-1).squeeze().tolist()
+        word_ids = encoded_input.word_ids()
+
+        # Initialize an empty list to hold the predictions for each original word
+        word_level_prediction = ['O'] * len(tokens)  # Assuming 'O' is the default label
+
+        for idx, word_id in enumerate(word_ids):
+            if word_id is not None and (idx == 0 or word_id != word_ids[idx - 1]):
+                # Assign the label of the first subtoken to the entire word
+                word_level_prediction[word_id] = model.config.id2label[prediction[idx]]
+
+        assert len(tokens) == len(word_level_prediction), "Length mismatch between tokens and predictions"
+        predictions.append(word_level_prediction)
+        actual_labels = [dataset.features['ner_tags'].feature.int2str(tag) for tag in example['ner_tags']]
+        mapped_actual_labels = map_model_tags_to_dataset_tags(actual_labels, manual_sub_mappings)   
+        labels.append(mapped_actual_labels)
+    metrics = compute_metrics(predictions, labels)
+    return metrics
+
 def evaluate_model(model_name, dataset):
     # Load the model and tokenizer using the pipeline
     nlp = pipeline("ner", model=model_name, tokenizer=model_name)
 
     predictions = []
     labels = []
+
+    manual_sub_mappings = model_name_to_manual_mapping[model_name]
+    check_model_tags(model_name, wnut2017_dataset)
 
     for example in dataset:
         tokens = example['tokens']
@@ -190,12 +297,14 @@ def evaluate_model(model_name, dataset):
         # Initialize predicted labels with 'O'
         pre_labels = ['O'] * len(tokenized_text)
         
+        
         # Update predicted labels based on ner_results
         for entity in ner_results:
             # Adjust the index to align with tokenized_text
             index = entity['index'] - 1  # Adjusting the index to match the tokenized text
             entity_word = entity['word'].replace("##", "")
-            
+            entity_tokens = nlp.tokenizer.tokenize(entity['word'])
+
             # Find the start index of the entity word in the tokenized text
             for i in range(index, min(index + 2, len(tokenized_text))):
                 if tokenized_text[i].endswith(entity_word):
@@ -203,7 +312,6 @@ def evaluate_model(model_name, dataset):
                     break
 
         # Align and map the predicted labels to the dataset labels
-        
         aligned_pre_labels = align_tokens(tokens, tokenized_text, pre_labels)
         mapped_pre_labels = map_model_tags_to_dataset_tags(aligned_pre_labels, manual_sub_mappings)
 
@@ -219,12 +327,13 @@ def main():
         'dslim/bert-base-NER',
         'dslim/bert-base-NER-uncased',
         'Babelscape/wikineural-multilingual-ner',
-        'tner/roberta-large-wnut2017'
+        'tner/roberta-large-wnut2017',
+        'dbmdz/bert-large-cased-finetuned-conll03-english',
     ]
 
     # Evaluate each model
     for model in models:
-        metrics = evaluate_model(model, wnut2017_dataset)
+        metrics = evaluate_model_w_orig_word_list(model, wnut2017_dataset)
         print(f"Metrics for {model}: {metrics}")
 
 if __name__ == "__main__":
